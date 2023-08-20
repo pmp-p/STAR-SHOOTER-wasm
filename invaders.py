19cd114b8390287fea39c8cbc5866cdcb7f9c244
Tanay-cmd <81357948+Tanay-cmd@users.noreply.github.com>
2023-08-20 23:13:03 +0530

    Update invaders.py

--- invaders.py
+++ invaders.py
@@ -9,11 +9,20 @@ class alien(pygame.sprite.Sprite):
         self.image = pygame.transform.rotozoom(self.image, 0, 0.7)
         self.rect = self.image.get_rect(midbottom=(x, y))
         self.speed = 1
+        self.color = color
         self.boundary = constraint
+    def score(self):
+        
+        if self.color == 'red':
+            return 10
+        if self.color == 'yellow':
+            return 30
+        if self.color == 'green':
+            return 50
         
         
     def update(self, direction):
-        self.rect.x += direction
+        self.rect.x += direction*self.speed
         
 
        
