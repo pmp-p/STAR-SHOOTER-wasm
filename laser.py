8cffde947223c7056a29545f6f1afd4f1fbd11ba
Tanay-cmd <81357948+Tanay-cmd@users.noreply.github.com>
2023-08-20 23:11:50 +0530

    Update laser.py

--- laser.py
+++ laser.py
@@ -1,14 +1,15 @@
 import pygame
 
 class Laser(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y, speed):
+    def __init__(self, pos_x, pos_y, speed, constraints):
         super().__init__() 
         self.image = pygame.image.load('Graphics//Charge_1.png')
         self.image = pygame.transform.rotozoom(self.image, 90, 0.4)
         self.rect = self.image.get_rect(midbottom=(pos_x, pos_y))
         self.speed = speed
+        self.constraints = constraints
     def destroy(self):
-        if self.rect.y < 0 or self.rect.y > 400:
+        if self.rect.y < 0 or self.rect.y > self.constraints + 3:
             self.kill()
     def update(self):
         self.rect.y -= self.speed
