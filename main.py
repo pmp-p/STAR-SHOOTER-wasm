827b293b555a0e2a0c011038bdcb3bf96c50b35b
Tanay-cmd <81357948+Tanay-cmd@users.noreply.github.com>
2023-08-20 23:11:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ from random import choice, randint
 from laser import Laser
 from player import Player
 from invaders import alien
+
+'''
+all classes definitions : 
+- laser
+Laser(pos_x, pos_y, speed, constraint)
+
+- Player
+Player(pos_x, pos_y, constraint)
+
+- invaders
+Alien(pos_x, pos_y, constraint, color)
+
+'''
+
 class Game:
     def __init__(self):
         self.game_active = False
@@ -10,13 +24,13 @@ class Game:
 
         self.lives = 3
         self.lives_surf = pygame.image.load('Graphics//spaceship.png')
-        self.lives_surf = pygame.transform.rotozoom(self.lives_surf, 0, 0.08)
-        self.lives_offset = screen_width - (40 * 3 + 20)
+        self.lives_surf = pygame.transform.rotozoom(self.lives_surf, 0, 0.1)
+        self.lives_offset = 200
         
         self.background = pygame.image.load('Graphics//backgound.png')
-        self.background = pygame.transform.scale2x(self.background)
+        self.background = pygame.transform.rotozoom(self.background,0, times_factor)
         
-        self.main_ship = Player(400, 320, 272*2 - 70)
+        self.main_ship = Player(400, screen_height, screen_width*times_factor - 70)
         self.main_ship_group = pygame.sprite.GroupSingle(self.main_ship)
         
         self.alienGroup = pygame.sprite.Group()
@@ -24,7 +38,7 @@ class Game:
         self.alien_lasers = pygame.sprite.Group()
         self.direction = 1
         #self.alien_setup()
-        self.text_font = pygame.font.Font('font\RETRO_SPACE_INV.ttf', 35)
+        self.text_font = pygame.font.Font('font\RETRO_SPACE_INV.ttf', 40)
         self.text_font1 = pygame.font.Font('font\RETRO_SPACE_INV.ttf', 25)
         self.score = 0
 
@@ -37,38 +51,72 @@ class Game:
     def alien_setup(self):
         if not self.alienGroup:
             number = randint(1,3)
+            color = None
             if number == 1:
-                for rows in range(4):
-                    for cols in range (4):
-                        self.aliens = alien( 100 + cols * 47,50 + rows*37,272*2 , 'red')
-                        self.alienGroup.add(self.aliens)
+                shape = ['xxzzzxx',
+                         ' yyzyy ',
+                         '  xyx  ',
+                         '   x  ']
+                for row_ind, rows in enumerate(shape) :
+                    for col_ind, cols in enumerate(rows):
+                        if cols != ' ':
+                            if cols == 'x':
+                               color = 'red'
+                            if cols == 'y':
+                                color = 'yellow'
+                            if cols == 'z':
+                                color = 'green'
+                            self.aliens = alien(100 + col_ind * 43,50*2 + row_ind*33,screen_width*times_factor , color)
+                            self.alienGroup.add(self.aliens)
             if number == 2:
-                for rows in range(2):
-                    for cols in range(7):
-                        self.aliens = alien(100 + cols * 47, 50 + rows*37, 272*2, 'yellow')
-                        self.alienGroup.add(self.aliens)
-            if number  == 3:
-                for rows in range(2):
-                    for cols in range(2):
-                        self.aliens = alien(100 + cols * 47, 50 + rows*37, 272*2, 'green')
-                        self.alienGroup.add(self.aliens)
+                shape = ['xyz zyx',
+                         ' xyzyx ',
+                         '  xyx  ',
+                         '   x   ']
+                for row_ind, rows in enumerate(shape) :
+                    for col_ind, cols in enumerate(rows):
+                        if cols != ' ':
+                            if cols == 'x':
+                               color = 'red'
+                            if cols == 'y':
+                                color = 'yellow'
+                            if cols == 'z':
+                                color = 'green'
+                            self.aliens = alien(100 + col_ind * 43,50*2 + row_ind*33,screen_width*times_factor , color)
+                            self.alienGroup.add(self.aliens)
+            if number == 3:
+                shape = ['xzzzzzx',
+                         'yyyyyyy ',
+                         'x xyx x ',
+                         '  xxx  ']
+                for row_ind, rows in enumerate(shape) :
+                    for col_ind, cols in enumerate(rows):
+                        if cols != ' ':
+                            if cols == 'x':
+                               color = 'red'
+                            if cols == 'y':
+                                color = 'yellow'
+                            if cols == 'z':
+                                color = 'green'
+                            self.aliens = alien(100 + col_ind * 43,50*2 + row_ind*33,screen_width*times_factor , color)
+                            self.alienGroup.add(self.aliens)
     
     def alien_downward(self):
         sprites = self.alienGroup.sprites()
         for sprite in sprites:
-            sprite.rect.y += 7
-    
+            sprite.rect.y += 20
+        
     def HUD(self):
         font = pygame.font.Font('font\RETRO_SPACE_INV.ttf', 25)
         text = font.render("SCORE : " + str(self.score),False, 'White')
         screen.blit(text, (0,0))
         text1 = font.render("Target : 600", False, 'White')
         
-        screen.blit(text1,(200,0))
-        x = self.lives_offset
+        screen.blit(text1,(screen_width - (40 * 3 + 70),0))
+        x = screen_width/2 - 70
         for lives in range(self.lives):
             screen.blit(self.lives_surf, (x, self.lives_surf.get_size()[1]))
-            x += 40
+            x += 50
                 
     def alien_position(self):
         sprites = self.alienGroup.sprites()
@@ -83,9 +131,10 @@ class Game:
     def collisions(self):
         if self.main_ship_group.sprite.laser_group:
             for laser in self.main_ship_group.sprite.laser_group:
-                if pygame.sprite.spritecollide(laser, self.alienGroup, True):
+                alien_collisions = pygame.sprite.spritecollide(laser, self.alienGroup, True)
+                for alien in alien_collisions:
                     laser.kill()
-                    self.score += 10
+                    self.score += alien.score()
         if self.alien_lasers:
             for laser in self.alien_lasers:
                 if pygame.sprite.spritecollide(laser, self.main_ship_group, False):
@@ -95,34 +144,49 @@ class Game:
                         print("========= GAME OVER ========== ")
                         self.game_state = 3
                         self.game_active = False
+                        
+            # if pygame.sprite.spritecollide(lasers, self.main_ship_group, True):
+            #     self.main_ship_group.empty()
+            #     self.alienGroup.empty()
+            #     self.game_active = False
+            #     self.game_state = 3
+            #     break
                 
     def alien_shoot(self):
         if self.alienGroup.sprites():
             sprites = self.alienGroup.sprites()
             self.laser_sound.play()
             random_sprite = choice(sprites)
-            alien_laser = Laser(random_sprite.rect.midbottom[0], random_sprite.rect.midbottom[1], -5)
-            self.alien_lasers.add(alien_laser)
+            selected_alien = random_sprite
+            if (selected_alien.color == 'red'):
+                alien_laser = Laser(random_sprite.rect.midbottom[0], random_sprite.rect.midbottom[1], -5, screen_height)
+                self.alien_lasers.add(alien_laser)
+                
+            if (selected_alien.color == 'yellow'):
+                alien_laser = Laser(random_sprite.rect.midbottom[0], random_sprite.rect.midbottom[1], -8, screen_height)
+                self.alien_lasers.add(alien_laser)
+
+            if (selected_alien.color == 'green'):
+                alien_laser = Laser(random_sprite.rect.midbottom[0], random_sprite.rect.midbottom[1], -10, screen_height)
+                self.alien_lasers.add(alien_laser)
             
     def state_manager(self):
         keys = pygame.key.get_pressed()
 
         if self.game_state == 1:
-            if keys[pygame.K_SPACE]:
+            if keys[pygame.K_b]:
                 self.game_active = True
             screen.blit(self.background, (0,0))
             text = self.text_font.render("STAR SHOOTER", False, 'Yellow')
-            text1 = self.text_font1.render("Press Space to start", False, 'White')
+            text1 = self.text_font1.render("Press B to start", False, 'White')
             screen.blit(text, (screen_width/2 - 150, screen_height/2 - 50))
             screen.blit(text1, (screen_width/2 - 150, screen_height/2 ))
-            
         elif self.game_state == 2:
             screen.blit(self.background, (0,0))
-            text3 = self.text_font.render("VICTORY!", False, 'Yellow')
+            text3 = self.text_font.render("VICTORYS", False, 'Yellow')
             screen.blit(text3, (screen_width/2 - 150 + 50, screen_height/2 - 50))
             score_final = self.text_font1.render("SCORE : " + str(self.score),False, 'White')
             screen.blit(score_final,(screen_width/2 - 150 + 50, screen_height/2 - 50 + 50))
-            
         elif self.game_state == 3:
             screen.blit(self.background, (0,0))
             text3 = self.text_font.render("GAME OVER", False, 'Yellow')
@@ -162,9 +226,9 @@ class Game:
 
 if __name__ == '__main__':
     pygame.init()
-
-    screen_width = 272*2
-    screen_height = 160*2
+    times_factor = 3
+    screen_width = 272*times_factor
+    screen_height = 160*times_factor
 
     screen = pygame.display.set_mode((screen_width, screen_height))
     title = pygame.display.set_caption('Star Shooter')
@@ -183,11 +247,15 @@ if __name__ == '__main__':
             if game.game_active:
                 if event.type == ALIENSHOOT:
                     game.alien_shoot()
-        
+                
+
         if game.game_active:
             game.run()
         else:
             game.state_manager()
+
+       
+
         pygame.display.flip()
         clock.tick(120)
             
